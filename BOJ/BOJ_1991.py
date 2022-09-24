f1bd0db53f2eb61ac8bee70b4a0b73a0770cff07
ChangMinL2E <dlckdals1004@naver.com>
2022-09-24 20:41:11 +0900

    0924 Tree

--- BOJ/BOJ_1991.py
+++ BOJ/BOJ_1991.py
@@ -4,25 +4,26 @@ N = int(input())
 
 lst = []
 Tree = []
-dic = {} # 알파벳 노드 넘버
+dic = {}  # 알파벳 노드 넘버
 
 for i in range(N):
-    a,b,c = map(str,input().split())
+    a, b, c = map(str, input().split())
     lst.append(a)
-    Tree.append([b,c])
+    Tree.append([b, c])
     dic[a] = i
 
 
-def pre(tree,par): # global 변수처럼 전위순회 사용
+def pre(par):  # global 변수처럼 전위순회 사용
     parent = par
-    children1 , children2 = Tree[dic[parent]][0],Tree[dic[parent]][1]
+    children1, children2 = Tree[dic[parent]][0], Tree[dic[parent]][1]
     print(parent, end='')
     if children1 != '.':
-        pre(Tree[dic[children1]],children1) # 좌측
+        pre(Tree[dic[children1]], children1)  # 좌측
     if children2 != '.':
-        pre(Tree[dic[children2]],children2) # 우측
+        pre(Tree[dic[children2]], children2)  # 우측
 
-def inorder(tree,par):
+
+def inorder(par):
     parent = par
     children1, children2 = Tree[dic[parent]][0], Tree[dic[parent]][1]
     if children1 != '.':
@@ -31,7 +32,8 @@ def inorder(tree,par):
     if children2 != '.':
         inorder(Tree[dic[children2]], children2)  # 우측
 
-def postorder(tree,par):
+
+def postorder(par):
     parent = par
     children1, children2 = Tree[dic[parent]][0], Tree[dic[parent]][1]
     if children1 != '.':
@@ -41,12 +43,8 @@ def postorder(tree,par):
     print(parent, end='')
 
 
-pre(Tree,lst[0])
+pre(lst[0])
 print()
-inorder(Tree,lst[0])
+inorder(lst[0])
 print()
-postorder(Tree,lst[0])
-
-
-
-
+postorder(lst[0])
