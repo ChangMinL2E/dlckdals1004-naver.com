1210b0377de2cf82cf8d65497149764c023fca29
unknown <dlckdals1004@naver.com>
2022-09-28 18:45:18 +0900

    0928

--- BOJ/BOJ_2206_2.py
+++ BOJ/BOJ_2206_2.py
@@ -1,22 +1,7 @@
-# BOJ 2206 벽 부수고 이동하기. Queue 써보자.
+# BOJ_2206 벽 부수고 이동하기 BFS
 
 import sys
-from collections import deque
-
-minimum = 100000000
-rows, cols = map(int, sys.stdin.readline().split())
-lst = [list(map(int, sys.stdin.readline().strip())) for _ in range(rows)]
-visited = [[0 for _ in range(cols)] for _ in range(rows)]
-visited[0][0] = 1
-# chance = 1
-#
-delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-BFS(lst, (0,0), 1)
-
-if minimum == 100000000:
-    minimum = -1
-print(minimum)
-
+sys.setrecursionlimit(1000000)
 
 def BFS(lst, which, chance):
     global minimum
@@ -29,25 +14,30 @@ def BFS(lst, which, chance):
     if visited[which[0]][which[1]] > minimum:
         return
 
-    x_now, y_now = which[0], which[1]
-
     # 길이 뚫린것부터!
     for dt in delta:
-        x, y = x_now + dt[0], y_now + dt[1]
+        x, y = which[0] + dt[0], which[1] + dt[1]
         if 0 <= x < rows and 0 <= y < cols and visited[x][y] == 0 and lst[x][y] == 0:
-            visited[x][y] = visited[x_now][y_now] + 1
+            visited[x][y] = visited[which[0]][which[1]] + 1
             BFS(lst, (x, y), chance)
             visited[x][y] = 0
 
-    # 길 막힌 경우도 전부 뚫어보자!
-    if chance == 1:
-        for dt in delta:
-            x, y = x_now + dt[0], y_now + dt[1]
-            if 0 <= x < rows and 0 <= y < cols and visited[x][y] == 0 and lst[x][y] == 1:
-                visited[x][y] = visited[x_now][y_now] + 1
-                BFS(lst, (x, y), 0)
-                visited[x][y] = 0
+        # 길 막힌 경우도 전부 뚫어보자!
+        elif chance == 1 and 0 <= x < rows and 0 <= y < cols and visited[x][y] == 0 and lst[x][y] == 1:
+            visited[x][y] = visited[which[0]][which[1]] + 1
+            BFS(lst, (x, y), 0)
+            visited[x][y] = 0
 
 
+minimum = 1000001
+rows, cols = map(int, sys.stdin.readline().split())
+lst = [list(map(int, sys.stdin.readline().strip())) for _ in range(rows)]
+visited = [[0 for _ in range(cols)] for _ in range(rows)]
+visited[0][0] = 1
+# chance = 1
+delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+BFS(lst, (0,0), 1)
 
-
+if minimum == 1000001:
+    minimum = -1
+print(minimum)
