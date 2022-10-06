e96fd737d02450f5205ce59b2efbf99b695b1d2d
ChangMinL2E <dlckdals1004@naver.com>
2022-10-06 17:56:04 +0900

    1006 try

---
+++ BOJ/BOJ_13905.py
@@ -0,0 +1,19 @@
+# BOJ_13905 세부 / dijkstra
+
+V, E = map(int,input().split())
+st, ed = map(int,input().split())
+
+edge = [list(map(int,input().split())) for _ in range(E)]
+
+def dijkstra():
+    U = []
+    P = [1e10]*(V+1)
+    D = [-1e10]*(V+1)
+    U[st] = 0
+
+    while True:
+        maximum = -1e10
+        for i in range(1,D):
+
+
+
