884cd55747372ad2661204fa5d87b3be555cfcf6
ChangMinL2E <dlckdals1004@naver.com>
2022-10-07 13:22:19 +0900

    1007

--- BOJ/BOJ_13905.py
+++ BOJ/BOJ_13905.py
@@ -5,15 +5,15 @@ st, ed = map(int,input().split())
 
 edge = [list(map(int,input().split())) for _ in range(E)]
 
-def dijkstra():
-    U = []
-    P = [1e10]*(V+1)
-    D = [-1e10]*(V+1)
-    U[st] = 0
-
-    while True:
-        maximum = -1e10
-        for i in range(1,D):
+# def dijkstra():
+#     U = []
+#     P = [1e10]*(V+1)
+#     D = [-1e10]*(V+1)
+#     U[st] = 0
+#
+#     while True:
+#         maximum = -1e10
+#         for i in range(1,D):
 
 
 
