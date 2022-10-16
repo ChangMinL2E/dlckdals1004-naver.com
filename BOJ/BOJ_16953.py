37e23cf0a54bfe5fc6a89924228ba00c6e868d4c
ChangMinL2E <dlckdals1004@naver.com>
2022-10-16 14:54:47 +0900

    1016 BFS

--- BOJ/BOJ_16953.py
+++ BOJ/BOJ_16953.py
@@ -3,23 +3,24 @@
 from collections import deque
 
 A,B = map(int,input().split())
-visited = [-1]*(1000000000+1)
+# visited = [-1]*(B+1)
 
-visited[A] = 1
-Queue = deque([A])
+# visited[A] = 1
+Queue = deque([(A,1)])
 result = -1
 while Queue:
     q = Queue.popleft()
-    if q == B:
+    if q[0] == B:
+        result = q[1]
         break
 
-    if 2*q <= B and visited[2*q] == -1:
-        visited[2*q] = visited[q]+1
-        Queue.append(2*q)
-    if 10*q+1 <= B and visited[10*q+1]:
-        visited[10*q+1] = visited[q]+1
-        Queue.append(10*q+1)
-print(visited[B])
+    if 2*q[0] <= B:# and visited[2*q] == -1:
+        #visited[2*q] = visited[q]+1
+        Queue.append((2*q[0],q[1]+1))
+    if 10*q[0]+1 <= B: # and visited[10*q+1] == -1:
+        #visited[10*q+1] = visited[q]+1
+        Queue.append((10*q[0]+1,q[1]+1))
+print(result)
 
 
 
