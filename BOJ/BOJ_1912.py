0167f004321c65f4bb3cf61b232a0210c08c87c9
unknown <dlckdals1004@naver.com>
2022-11-01 23:23:36 +0900

    1101

--- BOJ/BOJ_1912.py
+++ BOJ/BOJ_1912.py
@@ -1,1 +1,11 @@
-x
+# BOJ_1912 연속합
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+Lst = list(map(int, input().split()))
+lst = [Lst[0]]
+for i in range(len(Lst) - 1):
+    lst.append(max(lst[i] + Lst[i + 1], Lst[i + 1]))
+print(max(lst))
